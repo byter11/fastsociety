c6470fb46f1b09cc0797357c117d4fd117e8c479
byter11 <mohsinshk4026@gmail.com>
2021-12-09 20:04:44 +0500

    Event add modal

--- src/client/components/Events/AddEventBox.js
+++ src/client/components/Events/AddEventBox.js
@@ -1,58 +1,140 @@
-import {Container, Row, Col, Button, Image, Form, FloatingLabel} from 'react-bootstrap';
+import {Container, Row, Col, Button, Image, Form, FloatingLabel, Modal} from 'react-bootstrap';
 import { useFetchUser } from '../../hooks/user';
 import {useState, useEffect} from 'react';
 import Link from 'next/link';
 
 const AddEventBox = () => {
-    const {user, token} = useFetchUser();
+    const {user, loading, token} = useFetchUser();
     const [society, setSociety] = useState(null);
-
-    useEffect(()=>{
-        setSociety(
-            user ? user.societies.filter(s => s.role.createEvent)[0] : null
-        );
-        console.log(society);
+    const [dropDown, setDropDown] = useState(false);
+    const [eventData, setEventData] = useState({
+      
     });
 
-    const postEvent = (data) => {
+    useEffect(()=>{
+        if(loading)
+            return;
+
+        setSociety( (_) =>{
+            const society = user.societies.filter(s => s.role.createEvent)[0]
+            setEventData((old) => ({...old, Society_id: society.id}))
+            return society;
+        });
+        
+    }, [loading]);
+
+    const handleChange = e => {
+        const {name, value} = e.target;
+        if(name == 'Society_id')
+            setSociety(user.societies.filter(s=>s.id == value)[0])
+        if(name == 'startTime' || name == 'endTime' && eventData.date)
+            value = eventData.date + ' ' + value;
+        
+        setEventData((e) => ({...e, [name]: value}));
+        console.log(eventData);
+        console.log(name, value);
+    }
+
+
+    const handleSubmit = () => {
+
+    }
+
+    const postEvent = () => {
         fetch('/api/event/',{
             method: 'POST',
             headers: {'Content-Type': 'application/json', token: token},
-            body: data
+            body: eventData
         })
         .then((res) => console.log(res));
     }
 
-
+    if (!society) return <></>;
     return <>
-        {society &&
-            <Container style={{}}>
-                <Row className="justify-content-start">
-                    <Col style={{cursor:'pointer'}}>
-                        <Link href={`/society/${society.id}`}>
-                            <Image className="m-2" src={society.image || 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='} roundedCircle height={40} width={40} />
-                        </Link>
-                    </Col>
-                    <Col>
-                        <Form.Select className="my-2" style={{ border: 0, width: '10rem'}}>
-                            {user.societies.map((s,i) => 
-                                <option key={i} value={s.id}>{s.title}</option>    
-                            )}
-                        </Form.Select>
-                    </Col>
-                </Row>
+    <Container>
+        <div className="d-flex flex-wrap"
+        style={{cursor: 'pointer'}}>
+                <Link href={`/society/${society.id}`}>
+                    <Image className="my-2" src={society.image || 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='} roundedCircle height={40} width={40} />
+                </Link>
+            
+                <div 
+                    className='p-2 m-2 flex-fill d-flex bg-light rounded-pill border text-muted hover'
+                    onClick={()=>setDropDown(true)}
+                >
+                    Create an event...
+                </div>
+        </div>
+    </Container>
+
+    <Modal show={dropDown} onHide={()=>setDropDown(false)}>
+        <Modal.Body>
+            <Form onSubmit={postEvent}>
+                <div className="d-flex flex-wrap">
+                    <Link href={`/society/${society.id}`}>
+                        <Image className="m-2" src={society.image || 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='} roundedCircle height={40} width={40} />
+                    </Link>
+                    <Form.Select 
+                        className="my-2" 
+                        style={{ border: 0, width: '10rem'}}
+                        name="Society_id"
+                        onChange={handleChange}>
+                        {user.societies.map((s,i) => 
+                            <option key={i} value={s.id}>{s.title}</option>    
+                        )}
+                    </Form.Select>
+                </div>
+                <input
+                    type="date"
+                    name="date"
+                    value={null}
+                    onChange={handleChange}
+                    style={{border:'none'}}/>
+                <div className="m-2 d-flex flex-wrap justify-content-between">
+                    <input
+                        type="time"
+                        name="startTime"
+                        value={null}
+                        onChange={handleChange}
+                        style={{border: 'none'}}
+                        />
+                        -
+                    <input
+                        type="time"
+                        name="endTime"
+                        value={null}
+                        onChange={handleChange}
+                        style={{border: 'none'}}
+                        />
+                </div>
+                {/* <FloatingLabel label="Venue"> */}
+                    <Form.Control
+                        className="mb-2"
+                        size="sm"
+                        placeholder="Venue"
+                        type="text"
+                        name="venue"
+                        value={null}
+                        onChange={handleChange}
+                        />
+                {/* </FloatingLabel> */}
+                <FloatingLabel controlId="floatingTextarea2" label="Event text">
+                    <Form.Control
+                    name="textContent"
+                    onChange={handleChange}
+                    as="textarea"
+                    style={{ height: '100px' }}
+                    />
+                </FloatingLabel>
+
                 <Row>
-                    <Col>
-                    <FloatingLabel controlId="floatingTextarea2" label="Comments">
-                            <Form.Control
-                            as="textarea"
-                            placeholder="Leave a comment here"
-                            style={{ height: '100px' }}
-                            />
-                        </FloatingLabel></Col>
+                    <Col className=''><Button style={{width: '100%'}} className="my-1" variant="primary" type="submit">
+                        Post Event
+                    </Button></Col>
                 </Row>
-            </Container>
-        }
+            </Form>
+        </Modal.Body>
+    </Modal>
     </>
 }
 
