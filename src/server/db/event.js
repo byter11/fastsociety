923e167fcf2ba28378cc47766447c452b74ad3e1
byter11 <mohsinshk4026@gmail.com>
2021-12-10 14:47:09 +0500

    sql fixes

--- src/server/db/event.js
+++ src/server/db/event.js
@@ -20,8 +20,8 @@ const getMultiple = ({where={}, limit=10, offset=0, user=''}, cb) => {
 	const fields = 'e.id, e.textContent, e.createdOn, e.startTime, e.endTime, e.image, s.id, s.title, s.image';
 	db.query({
 		sql: `SELECT ${fields},
-		avg(r.stars) as rating, (SELECT stars FROM review WHERE User_id = ? AND Event_id = e.id) AS userRating
-		FROM event e
+		avg(r.stars) as rating, (SELECT stars FROM Review WHERE User_id = ? AND Event_id = e.id) AS userRating
+		FROM Event e
 		LEFT JOIN Society s ON s.id = e.Society_id
 		LEFT JOIN Review r ON r.Event_id = e.id
 		${conditions ? 'WHERE ' + conditions : ''}
