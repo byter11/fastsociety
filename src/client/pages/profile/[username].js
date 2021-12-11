35a5c55f3f71509e7358cf13862c3c73f45d5f6c
Gravity459 <59308294+Gravity459@users.noreply.github.com>
2021-12-11 22:58:10 +0500

    updated margin and size of image

--- src/client/pages/profile/[username].js
+++ src/client/pages/profile/[username].js
@@ -23,15 +23,20 @@ const Profile = () => {
 			});
 	}, [router.isReady]);
 
-	return <Layout>
-		<Container className='text-center'>
-					{user.image ? <Image src={user.image} roundedCircle />
-					: <Skeleton circle width={100} height={100}/>}
-					
-					<h1>{user.name || <Skeleton width={250}/>}</h1>
-					<pre>{user.email || <Skeleton width={200}/>}</pre>
-		</Container>
-	</Layout>
+	return (
+    <Layout>
+      <Container className="text-center mt-4">
+        {user.image ? (
+          <Image src={user.image} cover roundedCircle width={150} height={150} />
+        ) : (
+          <Skeleton circle width={100} height={100} />
+        )}
+
+        <h1>{user.name || <Skeleton width={250} />}</h1>
+        <pre>{user.email || <Skeleton width={200} />}</pre>
+      </Container>
+    </Layout>
+  );
 }
 
 export default Profile;
