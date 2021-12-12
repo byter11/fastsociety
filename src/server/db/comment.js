520f183d486c1f4f72c507acb5352cdf1879f68b
byter11 <mohsinshk4026@gmail.com>
2021-12-12 15:54:30 +0500

    comment get/post

--- src/server/db/comment.js
+++ src/server/db/comment.js
@@ -17,4 +17,24 @@ const insert = (values, cb) => {
   );
 };
 
-module.exports = { insert };
+const getMultiple = ({where={}, limit=10, offset=0, user=''}, cb) => {
+	limit = 10;
+    const {conditions, values} = buildConditions(where, 'c.');
+	db.query({
+		sql: `SELECT c.id commentId, c.textContent, u.id userId, c.createdOn
+          FROM Comment c
+          LEFT JOIN User u ON u.id = c.User_id
+		      ${conditions ? 'WHERE ' + conditions : ''}
+          ORDER BY createdOn desc
+          LIMIT ?, ?`, 
+		values: [...values, offset, limit],
+	},(error, results, fields) => {
+      console.log('db/comment.js: ', error, results);
+			if (error) cb(error);
+
+			cb(error, results, fields);
+		}
+	);
+}
+
+module.exports = { insert, getMultiple };
