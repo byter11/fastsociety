33c4767b2d6e44a94124a9b75d203d7138bf7280
Gravity459 <59308294+Gravity459@users.noreply.github.com>
2021-12-12 13:16:34 +0500

    minor change in query

--- src/server/db/comment.js
+++ src/server/db/comment.js
@@ -2,14 +2,14 @@ const db = require("./db");
 const { buildConditions } = require("../utils");
 
 const insert = (values, cb) => {
-  const { textContent, createdOn, User_id, Event_id } = values;
+  const { textContent,  User_id, Event_id } = values;
 
   db.query(
     {
       sql: `INSERT INTO Comment
-		(textContent, createdOn, User_id, Event_id)
-		VALUES (?, ?, ?, ?)`,
-      values: [textContent, createdOn, User_id, Event_id],
+		(textContent, User_id, Event_id)
+		VALUES (?, ?, ?)`,
+      values: [textContent, User_id, Event_id],
     },
     (error) => {
       cb(error);
